78593b6771e0e1b5d49f6d38cb32c8596907665e
ellenhong <e.hong@berkeley.edu>
2017-04-19 16:28:23 -0700

    team page done + optimized all images (#5)
    * finished team page
    
    * optimized all images
    
    * renamed officers.js to team.js
    
    * made changes to pull request

---
+++ pages/team.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PictureGrid from '../components/PictureGrid';
+import Title from '../components/Title';
+
+const team = () => (
+  <div className="officers">
+    <Title text="MEET OUR TEAM" />
+    <PictureGrid
+      pictures={[{
+        url: '/img/Andrew_Wang.jpg',
+        name: 'Andrew Wang',
+        title: 'President',
+      },
+      {
+        url: '/img/Brandon_Yee.jpg',
+        name: 'Brandon Yee',
+        title: 'Internal VP',
+      },
+      {
+        url: '/img/Teddy_Sun.jpg',
+        name: 'Teddy Sun',
+        title: 'External VP',
+      },
+      {
+        url: '/img/Brandon_Luo.jpg',
+        name: 'Brandon Luo',
+        title: 'Treasurer',
+      },
+      {
+        url: '/img/Phil_Kim.jpg',
+        name: 'Phil Kim',
+        title: 'Research Lead',
+      },
+      {
+        url: '/img/Raleigh_Lukas.jpg',
+        name: 'Raleigh Lukas',
+        title: 'Filtration Team Lead',
+      },
+      {
+        url: '/img/Katrina_Mazloomian.jpg',
+        name: 'Katrina Mazloomian',
+        title: 'Reactor Team Lead',
+      },
+      {
+        url: '/img/Jonathan_Ngan.jpg',
+        name: 'Jonathan Ngan',
+        title: 'Purification Team Lead',
+      },
+      {
+        url: '/img/Austin_Li.jpg',
+        name: 'Austin Li',
+        title: 'Logistics/Safety Lead',
+      }]}
+    />
+  </div>
+);
+
+export default team;
+
